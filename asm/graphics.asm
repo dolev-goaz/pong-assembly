7d8c24ee1df68c4343bc8f7d1a794a84dbc42ba2
dolevgo <www.dolev@gmail.com>
2023-09-03 15:52:46 +0300

    adjusted width and height

--- asm/graphics.asm
+++ asm/graphics.asm
@@ -251,7 +251,9 @@ GDrawRectangleBorder:
     GET_STACK_PARAM rcx, 4  ; x
     GET_STACK_PARAM r8, 3   ; y
     GET_STACK_PARAM r9, 2   ; width
+    sub r9, 1               ; width is one more for some reason?
     GET_STACK_PARAM rbx, 1  ; height
+    sub rbx, 1              ; height is one more for some reason?
     push rbx                ; height is in the stack for some reason
     call XDrawRectangle
 	CLEAR_STACK_PARAMS 1
@@ -272,7 +274,9 @@ GDrawRectangle:
 	GET_STACK_PARAM rcx, 4  ; x
 	GET_STACK_PARAM r8, 3   ; y
 	GET_STACK_PARAM r9, 2   ; width
+    sub r9, 1               ; width is one more for some reason?
 	GET_STACK_PARAM rbx, 1  ; height
+    sub rbx, 1              ; height is one more for some reason?
 	push rbx
 	call XFillRectangle
 	CLEAR_STACK_PARAMS 1
